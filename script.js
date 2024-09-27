f19e70f3ff91d9528101eac4913a0b2023480f03
AndreiLapotko <shudyx@gmail.com>
2024-09-27 12:46:41 +1000

    the home work is done

--- script.js
+++ script.js
@@ -3,8 +3,24 @@ import { celsiusToFahrenheit } from "./modules/convTemperature.js";
 import { calculateFallDistance } from "./modules/fallDistance.js";
 import { calculateAverage } from "./modules/average.js";
 import { concatStrings } from "./modules/concatenation.js";
+
 greet();
-console.log(celsiusToFahrenheit(20));
-console.log(calculateFallDistance(30));
-console.log("Среднее арифметическое чисел 10, 20 и 30 = " + calculateAverage(10, 20, 30));
-console.log(concatStrings("Hello", "World"));
+
+let celsiusTemp = Number(prompt("Введите температуру в градусах Цельсия: "));
+console.log(celsiusToFahrenheit(celsiusTemp));
+alert(celsiusToFahrenheit(celsiusTemp));
+
+let timeOfFall = Number(prompt("Введите время падения в секундах: "));
+console.log(calculateFallDistance(timeOfFall));
+alert(calculateFallDistance(timeOfFall));
+
+let a = Number(prompt("Введите первое число: "));
+let b = Number(prompt("Введите второе число: "));
+let c = Number(prompt("Введите третье число: "));
+console.log(`Среднее арифметическое чисел ${a}, ${b} и ${c} = ` + calculateAverage(a, b, c));
+alert(`Среднее арифметическое чисел ${a}, ${b} и ${c} = ` + calculateAverage(a, b, c));
+
+let str1 = prompt("Введите первое слово: ");
+let str2 = prompt("Введите второе слово: ");
+console.log(concatStrings(str1, str2));
+alert(concatStrings(str1, str2));
